ffd99ddecbc858d927b170ba12873663783f31c4
Praveensaich <97016531+Praveensaich@users.noreply.github.com>
2023-08-03 16:51:07 +0530

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -1,6 +1,12 @@
-import pandas
 import streamlit
-streamlit.header(" Table of Fruits ")
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect("Pick fruits ", list(my_fruit_list.index),["Strawberries"])
-streamlit.dataframe(my_fruit_list)
+import pandas
+streamlit.title(" Swiggy Instamart ")
+streamlit.header(" Items ")
+streamlit.text(" Cakes ")
+streamlit.text(" Drinks ")
+streamlit.text(" Chocolates ")
+streamlit.header("Fruits")
+fruits = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(fruits)
+
+
