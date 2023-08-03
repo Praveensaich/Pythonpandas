c8992a3871ebf8e5d4941bedc43bf11ca15a93e4
Praveensaich <97016531+Praveensaich@users.noreply.github.com>
2023-08-03 16:12:21 +0530

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -2,5 +2,5 @@ import pandas
 import streamlit
 streamlit.header(" Table of Fruits ")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-stremlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
 streamlit.dataframe( my_fruit_list )
