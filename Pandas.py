7368daf265c307785414c209ea328d3f80615fe1
Praveensaich <97016531+Praveensaich@users.noreply.github.com>
2023-08-03 18:15:24 +0530

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -7,7 +7,7 @@ streamlit.text(" Drinks ")
 streamlit.text(" Chocolates ")
 streamlit.header("Fruits")
 fruits = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect("Pick a Fruit: ")
+streamlit.multiselect("Pick a Fruit: ", list(fruits.index))
 streamlit.dataframe(fruits)
 
 
