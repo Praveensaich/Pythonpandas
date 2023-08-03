fe234473b2ad64e4edbe087d81e8e528255e504c
Praveensaich <97016531+Praveensaich@users.noreply.github.com>
2023-08-03 16:01:38 +0530

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -2,4 +2,4 @@ import pandas
 import streamlit
 streamlit.header(" Table of Fruits ")
 my_fruits_list = pandas.readcsv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe( my_fruits )
+streamlit.dataframe( my_fruits_list )
